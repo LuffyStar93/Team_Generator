f741fb5ad4ed51e0c96ebe6fc5e566ae67a5ffca
Maxime Chagnon <luffy-star-93@hotmail.fr>
2021-01-18 17:12:25 +0100

    application fonctionnelle (a améliorer)

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-
 players = [];
+nbrPerGroup = Number;
 
-// Remplissage du tableau des joueurs si enregistrer dans le localStorage
 let setplayers = () => {
-    if(localStorage.getItem('Players') === null){
+    if (localStorage.getItem('Players') === null) {
         players = [];
 
-    }else {
+    } else {
         players = localStorage.getItem('Players').split(',');
     }
 }
 setplayers()
 
-console.log("Players = ", players);
-console.log(localStorage);
-
-
-// Fonction d'ajout de joueurs
-
-addPlayer = function() {
+addPlayer = function () {
     playerName = document.querySelector('#character').value;
     if (playerName !== "") {
         if (players.indexOf(playerName) == -1) {
@@ -26,31 +19,63 @@ addPlayer = function() {
             localStorage.setItem("Players", players);
         }
     }
+    console.log('nbr per groupe = ', nbrPerGroup);
     document.querySelector('#character').value = "";
     document.querySelector('#character').focus();
+    display_players();
 };
 
 // fonction pour reset les joueurs
-
 let clearAll = () => {
     players = [];
     localStorage.removeItem('Players');
+    display_players();
 }
 
-
-// fonction pour afficher la liste des joueurs
-
+// Afficher la liste des joueurs
 let display_players = () => {
     if (players.length > 0) {
-        pl = "Player List: ";
+        pl = "";
         for (i = 0; i < players.length; i++) {
             pl += players[i] + ", ";
         }
         pl += ",";
-    }
-    else {
-        pl = "No players in the player list!\nType a name in the text box below and hit enter to add some!";
+    } else {
+        pl = "Ajoutez des noms au tableau";
     }
     document.querySelector("#players_list").innerHTML = pl.split(", ,")[0];
 }
 display_players();
+
+
+
+let groupDisplay = () => {
+
+    
+    const nbrPerGroup = document.querySelector('#nbrPerGroup').value;
+    const container = document.querySelector("#groups");
+
+    if(nbrPerGroup === null || nbrPerGroup === undefined || nbrPerGroup <= 0 ){
+        return 
+    } else {
+
+        while (players.length > 0) {
+            const div = document.createElement('div');
+            div.classList.add("group")
+            div.innerHTML = `<p> Ce groupe contient : <br/> </p>`
+            const p = document.createElement('p');
+            for (let i = 0; i < nbrPerGroup; i++) {
+                if (players.length === 0) {
+                    break
+                }
+                const span = document.createElement('span');
+                const index = Math.floor(Math.random() * players.length)
+                span.innerHTML = `${players[index]} <br/>`
+                span.classList.add("people");
+                div.appendChild(span);
+                players.splice(index, 1);
+            }
+            container.appendChild(div)
+        }
+    }
+}
